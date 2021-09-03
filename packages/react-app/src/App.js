792a2b70fddc34c78e7ed2962076d6ecbcb44492
silvercondor <biocrypto@protonmail.com>
2021-09-03 19:57:57 +0800

    Screw eslint

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,3 +1,4 @@
+ /* eslint-disable */ 
 import React, {useState, useEffect} from "react";
 import { Contract } from "@ethersproject/contracts";
 import { getDefaultProvider } from "@ethersproject/providers";
